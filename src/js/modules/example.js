b77d32547a8b5eda7ffaf929503c5c87618cb917
Abdukulov Talgat <abdukulov.talgat@gmail.com>
2022-05-08 22:35:41 +0500

    block example. Разметка и стилистика

--- src/js/modules/example.js
+++ src/js/modules/example.js
@@ -1,12 +1,12 @@
-const afterBox = document.querySelector('.demo__inner--after');
+const beforeBox = document.querySelector('.demo__inner--after');
 const toggler = document.querySelector('.demo__toggler');
 
 
 toggler.addEventListener('click', () => {
     toggler.classList.toggle('demo__toggler--after');
 
-    afterBox.style.width =
-        afterBox.style.width === '0%' ? '100%' : '0%';
+    beforeBox.style.width =
+        beforeBox.style.width === '0%' ? '100%' : '0%';
 })
 
 
